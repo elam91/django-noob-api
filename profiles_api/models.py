0e07f3add873cf4912df60f3c18bbe24c085e4e6
elam91 <elam91@gmail.com>
2021-06-27 12:35:24 +0300

    Added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
-    def create_user(self, email,name,password=None):
+
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError("Users must have an email address")
@@ -18,15 +19,16 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self,email,name,password):
+    def create_superuser(self, email, name, password):
         """Create a new superuser with given details"""
-        user = self.create_user(email,name,password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
         return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
@@ -51,3 +53,18 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
